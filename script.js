3aba5ae9202306b32fb4ce2032ca7722a1fc8753
ddanny16 <dammydan16@gmail.com>
2022-05-14 20:10:01 +0200

    .console.log(mySurname.slice(2))

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ console.log(myName.toUpperCase())
 console.log(mySurname.toLowerCase())
 console.log(myName.slice(0,4))
 console.log(myName.slice(4,myName.length))
+console.log(myName.slice(2,-2))
+console.log(mySurname.slice(2))
+
