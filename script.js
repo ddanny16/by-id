6ad80859b2151b39681ba6fc10ceee79949b4576
ddanny16 <dammydan16@gmail.com>
2022-05-14 19:53:54 +0200

    console.log(myName.toUpperCase())

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ let mySurname = "AHMED"
 
 console.log("i am",myName)
 console.log("my surname is",mySurname)
-console.log(myName.toUpperCase)
+console.log(myName.toUpperCase())
+console.log(mySurname.toLowerCase())
