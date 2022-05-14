c47f6b501be4a3e2a3feb0a1b1cd4dab08c66a1b
ddanny16 <dammydan16@gmail.com>
2022-05-14 20:15:27 +0200

    console.log(myName.length);

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ console.log("my surname is",mySurname)
 console.log(myName.toUpperCase())
 console.log(mySurname.toLowerCase())
 console.log(myName.slice(0,4))
-console.log(myName.slice(4,myName.length))
+console.log(myName.length);
 console.log(myName.slice(2,-2))
 console.log(mySurname.slice(2))
+
 
